7cf7f0eb52a9790ac4f15e2e795075f74e59824a
Pratibha_Tyagi <115cs2425@rkgit.edu.in>
2026-05-27 12:03:21 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,9 @@
+import numpy as np
+listarr =np.array([[2,89,65],[6,7,8],[8,5,2]])
+print(listarr.shape)
+print(listarr[0])
+print(listarr[1])
+print(listarr[2])
+print(listarr.dtype)
+print(listarr.shape)
+print(listarr.size)
